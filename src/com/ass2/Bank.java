b9a2ecf45150f6037bb87439dbf79cac8bab0ea7
xuandai314@gmail.com <Chopin96@github.com>
2018-02-03 23:23:44 -0500

    commit

--- src/com/ass2/Bank.java
+++ src/com/ass2/Bank.java
@@ -1,27 +1,77 @@
 package com.ass2;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Bank {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Bank");
 
         LinkedList<Customer> customerDatabase = new LinkedList<>();
 
-        customerDatabase.add(new Customer("Xuan",1234,0));
+        while(true){
+            printMessage();
+            Scanner scanner = new Scanner(System.in);
+            int userInput = scanner.nextInt();
 
-        customerDatabase.get(0).createCheckingAccount(599,50000);
+            if(userInput==1){
+                System.out.println("Please Enter Customer Informations: name, ID, discount rate");
+                String newName = scanner.next();
+                int newId = scanner.nextInt();
+                double newDiscountPercentage = Double.parseDouble(scanner.next());
+                customerDatabase.add(new Customer(newName,newId,newDiscountPercentage));
+            }
+            else if(userInput==2){
+                System.out.println("Please Enter a Customer ID");
+                int customerID = scanner.nextInt();
 
-        customerDatabase.get(0).createSavingAccount(589,10000);
+                System.out.println("Press S for saving account C for checking Account");
 
-        customerDatabase.get(0).printAllAccounts();
+            }
+            else if(userInput==3){
+                System.out.println("Please Enter a Customer ID ");
+                System.out.println("Please Enter Bank ID");
 
-        customerDatabase.get(0).getAccountList().get(0).transferMoney(10,customerDatabase.get(0).getAccountList().get(1));
+            }
+            else if(userInput==4){
+                System.out.println("Please Enter Customer ID");
+                System.out.println("Please Enter Bank ID");
+                System.out.println("Please Enter Amount");
 
-        customerDatabase.get(0).printAllAccounts();
+            }
+            else if(userInput==5){
+                System.out.println("Please Enter Customer ID");
+                System.out.println("Please Enter Source Bank ID");
+                System.out.println("Please Enter Destination Bank ID");
+                System.out.println("Please Enter Transfer Amount");
+
+
+            }
+            else if(userInput==6){
+
+                System.exit(0);
+
+            }
+            else {
+                System.out.println("Invalid input");
+            }
+
+
+        }
 
 
     }
+
+    public static void printMessage(){
+        System.out.println("Welcome to the Bank");
+        System.out.println("1.Create Customer");
+        System.out.println("2.Create Bank Account");
+        System.out.println("3.Get Balance");
+        System.out.println("4.Deposit");
+        System.out.println("5.Make a Transfer");
+        System.out.println("6.Quit");
+
+    }
+
 
 }
